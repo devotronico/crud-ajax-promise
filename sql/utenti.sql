4510abf9cf31c6dc74bbe7b9f999d01021b73a21
Daniele Manzi <dmanzi83@gmail.com>
2019-01-05 23:36:59 +0100

    commit #3.1

--- sql/utenti.sql
+++ sql/utenti.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 23, 2018 alle 19:12
+-- Creato il: Gen 05, 2019 alle 23:35
 -- Versione del server: 10.1.36-MariaDB
 -- Versione PHP: 7.2.11
 
@@ -31,8 +31,22 @@ SET time_zone = "+00:00";
 CREATE TABLE `utenti` (
   `id` int(11) NOT NULL,
   `firstname` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastname` text COLLATE utf8mb4_unicode_ci NOT NULL
+  `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dump dei dati per la tabella `utenti`
+--
+
+INSERT INTO `utenti` (`id`, `firstname`, `email`, `city`, `phone`) VALUES
+(1, 'Daren McGlynn', 'Lysanne43@hotmail.com', 'Lake Janick', '1-857-929-1516 x2210'),
+(2, 'Grayson Osinski', 'Tyshawn.Yost@gmail.com', 'Bettymouth', '1-422-952-4374 x27481'),
+(3, 'John Abshire', 'Hank0@yahoo.com', 'Muellerville', '(959) 109-1786 x4317'),
+(4, 'Dr. Leonora Upton', 'Hilario20@hotmail.com', 'Jovanmouth', '057-395-4521'),
+(5, 'Dr. Leon Durgan', 'Anastasia34@gmail.com', 'Arvillaberg', '(276) 785-6971 x2007'),
+(6, 'Otho Smith', 'Lilly.Williamson@gmail.com', 'West Mallieview', '385.776.9511');
 
 --
 -- Indici per le tabelle scaricate
@@ -52,7 +66,7 @@ ALTER TABLE `utenti`
 -- AUTO_INCREMENT per la tabella `utenti`
 --
 ALTER TABLE `utenti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
